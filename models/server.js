98ed693b7879b11d8b8ae4e9b1ec3926329f3620
Jefferson Becerra <jeffersonbecerra335@gmail.com>
2023-03-28 20:51:59 -0500

    cor

--- models/server.js
+++ models/server.js
@@ -43,11 +43,28 @@ class Server {
 
     middlewares() {
         this.app.use(express.json())
-        this.app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "*")
-            res.setHeader("Access-Control-Allow-Methods", "*")
-            res.setHeader("Access-Control-Allow-Headers", "*")
-        })
+        const allowCors = fn => async (req, res) => {
+            res.setHeader('Access-Control-Allow-Credentials', true)
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            // another common pattern
+            // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+            res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+            res.setHeader(
+              'Access-Control-Allow-Headers',
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+            )
+            if (req.method === 'OPTIONS') {
+              res.status(200).end()
+              return
+            }
+            return await fn(req, res)
+          }
+          
+          const handler = (req, res) => {
+            const d = new Date()
+            res.end(d.toString())
+          }
+        this.app.use(allowCors)
         this.app.use(cors())
         /* this.app.use(fileUpload({
             useTempFiles: true,
