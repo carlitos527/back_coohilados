9c033b446bfde797346a5c866fa6013afa366c9c
Jefferson Becerra <jeffersonbecerra335@gmail.com>
2023-03-28 21:04:37 -0500

    cors

--- models/server.js
+++ models/server.js
@@ -43,18 +43,9 @@ class Server {
 
     middlewares() {
         this.app.use(express.json())
-        this.app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Credentials', true)
-            res.setHeader('Access-Control-Allow-Origin', '*')
-            // another common pattern
-            // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-            res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-            res.setHeader(
-              'Access-Control-Allow-Headers',
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-            )
-        })
-        this.app.use(cors())
+        this.app.use(cors({
+            origin:"*"
+        }))
         /* this.app.use(fileUpload({
             useTempFiles: true,
             tempFileDir: '/tmp/',
